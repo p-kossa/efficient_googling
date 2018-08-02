db7be9fa01688c03261f5d82b2b41cfbfe112f5d
p-kossa <kevossa526@gmail.com>
2018-08-02 17:15:45 -0400

    minor changes

--- lucky.py
+++ lucky.py
@@ -11,8 +11,8 @@ r.raise_for_status()
 # retrives top search result links
 soup = bs4.BeautifulSoup(r.text)
 
-#opens a tab for each search result
+# opens a tab for each search result
 links = soup.select('.r a')
-numOpen = min(5, len(links))
+numOpen = min(8, len(links))
 for i in range(numOpen):
     webbrowser.open('http://google.com' + links[i].get('href'), new=1)
